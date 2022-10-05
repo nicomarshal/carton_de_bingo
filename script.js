725d9d38185b601ba9c2ef4b6e8e5a1d5dda5345
Nico Ledesma <dev.ledesmanicolas@gmail.com>
2022-10-05 01:21:27 -0300

    v2

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const createLockers = () => {
 		fragment.appendChild(locker);
 	}
 	bingoCard.appendChild(fragment);
-	generateNumbers();
+	generateMatrix();
 }
 
 let x; //Variable donde almacenamos valores aleatorios enteros.
@@ -20,21 +20,19 @@ let n; //Sirve para aumentar de 10 en 10 el rango de numeros que puede tomar "x"
 
 
 let matrix = [];
-const generateNumbers = () => {
+const generateMatrix = () => {
 	let x = 0;
 	let n = 0;
 	let rPos;
 	for (let i = 0; i < 9; i++) {
 		//For i es para las columnas
 		let col = [];
-		let init = Math.floor(Math.random()*2); //Toma valores 1 o 2.
 		for (let j = 0; j < 3; j++) {
 			//For j es para las filas
 			if (j === 0) {
 				rPos = Math.floor(Math.random()*col.length);
 				x = Math.floor(Math.random()*10 + n);
 				col[j] = x;
-		
 			}
 			if (j > 0) {
 				do {
@@ -50,14 +48,130 @@ const generateNumbers = () => {
 			}
 		}		
 	}
-	console.log(matrix);
-	deleteNumbersY();
+	console.log("Matrix: ", matrix);
+	rotateMatrix();
+}
+
+let rotMatrix = [[], [], []];
+const rotateMatrix = () => {
+	if (rotMatrix[0].length === 0) {
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 9; j++) {
+				rotMatrix[i][j] = matrix[j][i];
+			}	
+		}	
+		console.log("rotMatrix: ", rotMatrix);
+		deleteNumbersX();
+	}
+	else {
+		for (let i = 0; i < 9; i++) {
+			for (let j = 0; j < 3; j++) {
+				matrix[i][j] = rotMatrix[j][i];
+			}	
+		}	
+		console.log("Matrix: ", matrix);
+		deleteNumbersY();		
+	}
+}
+
+const deleteNumbersX = () => {
+	let rPos;
+	for (let i = 0; i < 3; i++) {
+		let rRef = [];
+		for (let j = 0; j < 4; j++) {
+			if (j === 0) {
+				rPos = Math.floor(Math.random()*(rotMatrix[i].length));						
+				rotMatrix[i][rPos] = "";
+				rRef.push(rPos);
+			}
+			if (j > 0) {
+				do {
+					rPos = Math.floor(Math.random()*(rotMatrix[i].length));					
+				} while (rRef.indexOf(rPos) !== -1);	
+
+				rotMatrix[i][rPos] = "";
+				rRef.push(rPos);			
+			}
+		}
+	}	
+	console.log("rotMatrix: ", rotMatrix);
+	rotateMatrix();
 }
 
 const deleteNumbersY = () => {
 	let rPos;
+	for (let i = 0; i < 9; i++) {
+		//For i es para las columnas
+		let rRef = [];
+		let counterY = Math.floor(Math.random()*2 + 1); //Toma los valores 1 o 2.
+		for (let j = 0; j < counterY; j++) {
+			//For j es para las filas
+			if (j === 0) {
+				rPos = Math.floor(Math.random()*matrix[i].length);
+				matrix[i][rPos] = "";
+				rRef.push(rPos);	
+			}
+
+			if (j > 0) {
+				do {
+					rPos = Math.floor(Math.random()*(matrix[i].length));					
+				} while (rRef.indexOf(rPos) !== -1);
+
+				matrix[i][rPos] = "";
+			}
+		}		
+	}
+	console.log("Matrix: ", matrix);
+	playBingo();	
+}
+
+
+/*const deleteNumbersY = () => {
+	let rPos;
 	let rRef;
-	let counterX = 14;
+	let rotMatrix = [[], [], []];
+	for (let i = 0; i < 9; i++) {
+		//For i es para las columnas
+		let counterY = Math.floor(Math.random()*2 + 1); //Toma los valores 1 o 2.
+		for (let j = 0; j < counterY; j++) {
+			//For j es para las filas
+			if (j === 0) {
+				rPos = Math.floor(Math.random()*matrix[i].length);
+				if (rotMatrix[rPos].length < 4) {
+					matrix[i][rPos] = "";
+					rotMatrix[rPos].push("");	
+				}else {
+					do {
+						rRef = rPos;
+						rPos = Math.floor(Math.random()*matrix[i].length);
+					} while ((rPos === rRef) && (rotMatrix[rPos].length >= 4));	
+					matrix[i][rPos] = "";
+					rotMatrix[rPos].push("");									
+				}
+			}
+
+			if (j > 0) {
+				do {
+					rRef = rPos;
+					rPos = Math.floor(Math.random()*matrix[i].length);
+				} while ((rPos === rRef) && (rotMatrix[rPos].length >= 4));
+
+
+				matrix[i][rPos] = "";
+				rotMatrix[rPos].push(matrix[i][rPos]);	
+			}
+		}		
+	}
+	console.log(rotMatrix);
+	playBingo();	
+}*/
+
+/*const deleteNumbersY = () => {
+	let rPos;
+	let rRef;
+	let row0 = [];
+	let row1 = [];
+	let row2 = [];
 	for (let i = 0; i < 9; i++) {
 		//For i es para las columnas
 		let counterY = Math.floor(Math.random()*2);
@@ -65,26 +179,48 @@ const deleteNumbersY = () => {
 			//For j es para las filas
 			if (j === 0) {
 				rPos = Math.floor(Math.random()*matrix[i].length);
-				matrix[i][rPos] = "";
-				if (counterY === 0) {
-					counterX -= 2;
-				}else {
-					counterX--;
+				if (rPos === 0 && row0.length < 4) {
+					matrix[i][rPos] = "";
+					row0.push(matrix[i][rPos]);	
 				}
-								
+				if (rPos === 1 && row1.length < 4) {
+					matrix[i][rPos] = "";
+					row1.push(matrix[i][rPos]);	
+				}
+				if (rPos === 2 && row2.length < 4) {
+					matrix[i][rPos] = "";
+					row2.push(matrix[i][rPos]);	
+				}
 			}
-			if (j > 0 && counterY < 1 && counterX > 0) {
+
+			if (j > 0 && counterY < 1) {
 				do {
 					rRef = rPos;
 					rPos = Math.floor(Math.random()*matrix[i].length);
 				} while (rPos === rRef);
-				matrix[i][rPos] = "";
+
+				if (rPos === 0 && row0.length < 4) {
+					matrix[i][rPos] = "";
+					row0.push(matrix[i][rPos]);	
+				}
+				if (rPos === 1 && row1.length < 4) {
+					matrix[i][rPos] = "";
+					row1.push(matrix[i][rPos]);	
+				}
+				if (rPos === 2 && row2.length < 4) {
+					matrix[i][rPos] = "";
+					row2.push(matrix[i][rPos]);	
+				}
+
 				counterY++;
 			}
 		}		
 	}
+	console.log(row0);
+	console.log(row1);
+	console.log(row2);
 	playBingo();	
-}
+}*/
 
 /*const deleteNumbersY = () => {
 	let rPos;
